9c8ce0db927218a27a19c7a207a679e2dcad3862
meezumi <aaryank3000@gmail.com>
2025-02-19 20:40:28 +0530

    enhance transaction history handling, and improve error handling

--- banking/components/MobileNav.tsx
+++ banking/components/MobileNav.tsx
@@ -33,13 +33,13 @@ const MobileNav = ({ user }: MobileNavProps) => {
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-white">
           <Link href="/" className="cursor-pointer flex items-center gap-1 px-4">
-            <Image 
+            {/* <Image 
               src="/icons/logo.svg"
               width={34}
               height={34}
               alt="Horizon logo"
-            />
-            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
+            /> */}
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">FinEdge</h1>
           </Link>
           <div className="mobilenav-sheet">
             <SheetClose asChild>
